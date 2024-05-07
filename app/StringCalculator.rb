9a7d8276475de1f519ed9163a21d9dd413c3ea39
Manjunath <manjunath724@gmail.com>
2024-05-07 17:01:16 +0530

    Add test cases to handle negative input

--- app/StringCalculator.rb
+++ app/StringCalculator.rb
@@ -4,6 +4,7 @@ class StringCalculator
     numbers.gsub!('\n', ',')
 
     validate_size(numbers)
+    validate_negative(numbers)
 
     numbers.split(',').map(&:to_i).inject(0) do |sum, num|
       sum + num
@@ -15,6 +16,11 @@ class StringCalculator
   def validate_size(numbers)
     numbers_count = numbers.scan(/\d+/).size
     commas_count = numbers.scan(/,/).size
-    raise StandardError.new('Invalid Input') unless numbers_count > commas_count
+    raise StandardError.new('invalid input') unless numbers_count > commas_count
+  end
+
+  def validate_negative(numbers)
+    negatives = numbers.scan(/-\d+/)
+    raise StandardError.new("negative numbers are not allowed #{negatives.join(', ')}") unless negatives.empty?
   end
 end
